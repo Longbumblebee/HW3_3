d940b1f07e0e265a5bc1cdc85a7a5e935bcbd4b8
Владислав Кузнецов <kz-vlad@mail.ru>
2021-03-09 00:06:19 +0300

    Добавьте файлы проекта.

---
+++ HW3_3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW3_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string tempString;
+            while (true)
+            {
+                Console.WriteLine("Write something:");
+                tempString = Console.ReadLine();
+                char[] charArray = tempString.ToCharArray();
+                Array.Reverse(charArray);
+                Console.WriteLine(new string(charArray));
+            }
+        }
+    }
+}
